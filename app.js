c8d466b4dbc4b2a1af295c48718e01d168870a58
Fiyinfolu Odeleke <fiyinfolu.odeleke@longbridgetech.com>
2021-11-04 10:28:37 +0100

    changed the connection port

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/api/v1/products', productRouter)
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 4000
 
 const start = async () => {
     try {
